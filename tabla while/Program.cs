5bb140cfbfe988f3c08286193e071e5a1e5cb466
Vanidad <57409926+vanesa33@users.noreply.github.com>
2021-02-22 16:59:32 -0300

    TABLA DE MULTIPLICAR CON WHILE

---
+++ tabla while/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace TABLA_DE_MULTIPLICAR_CON_WHILE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM, I, RESUL;
+            string linea;
+            Console.Write("DIGITE NÚMERO:"); linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+            I = 1;
+            while ((I <= 12))
+            {
+                RESUL = NUM * I;
+                Console.WriteLine("{0} * {1} = {2}", NUM, I, RESUL);
+                I = I + 1;
+            }
+            Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
